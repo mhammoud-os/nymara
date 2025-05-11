3fa3c3d96abf608ff78417d4b66e1003cd08ac7d
Malek Hammoud <362858318@gotvdsb.ca>
2025-05-11 12:01:36 -0400

    padding

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,4 +1,3 @@
-
 import React, { useRef } from 'react';
 import { ArrowDown, Youtube, FileText } from 'lucide-react';
 
@@ -13,7 +12,7 @@ const HeroSection = () => {
   };
 
   return (
-    <section ref={heroRef} className="relative min-h-screen flex items-center justify-center overflow-hidden pt-20 px-4 md:px-6">
+    <section ref={heroRef} className="relative min-h-screen flex items-center justify-center overflow-hidden pt-20 md:pt-32 px-4 md:px-6">
       {/* Flooded road background image */}
       <div className="absolute inset-0 z-0">
         <img 
@@ -82,3 +81,4 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
+
